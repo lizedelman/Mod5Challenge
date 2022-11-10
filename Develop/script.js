397c7be1532c18a385056165072689a1ac765613
Wild Carrot <edelman.eliz@gmail.com>
2022-11-10 11:00:48 -0700

    Create and fix color code time block functiongit add .

--- Develop/script.js
+++ Develop/script.js
@@ -1,53 +1,56 @@
 //Issues:
 //1. The colorcode for time period doesn't work - not being called
 
-var hourBlock = document.getElementById(".row");
-
 //Sets current day at top of page
 var currentDay = dayjs().format("MMM D, YYYY");
 $("#currentDay").text(currentDay);
 
-//Sets current time at top of page
-var currentTime = dayjs().format("hh:mm");
-console.log(currentTime);
-
 // load html before js
 $(document).ready(function () {
-  $(
-    function () {
-      //Save button function
-      $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".col-md-9").val();
-        var time = $(this).parent().attr("id");
-        localStorage.setItem(time, text);
-      });
+  //Get current time
+  var currentTime = dayjs().format("hh");
+  console.log(currentTime);
 
-      $("#8 .col-md-9").val(localStorage.getItem("8"));
-      $("#9 .col-md-9").val(localStorage.getItem("9"));
-      $("#10 .col-md-9").val(localStorage.getItem("10"));
-      $("#11 .col-md-9").val(localStorage.getItem("11"));
-      $("#12 .col-md-9").val(localStorage.getItem("12"));
-      $("#13 .col-md-9").val(localStorage.getItem("13"));
-      $("#14 .col-md-9").val(localStorage.getItem("14"));
-      $("#15 .col-md-9").val(localStorage.getItem("15"));
-    },
+  //color codes the time blocks
+  var hourBlock = $(".row");
 
-    //Should color code the hour blocks - NOT working
-    function setHourBlockClass() {
-      document.querySelectorAll(".row").forEach(setHourBlockClass);
-      if (hourBlock > currentTime) {
-        $(this).addClass("future");
-        // $(this).removeClass("past");
-        // $(this).removeClass("present");
-      } else if (hourBlock === currentTime) {
-        $(this).addClass("present");
-        // $(this).removeClass("past");
-        // $(this).removeClass("future");
-      } else {
-        $(this).addClass("past");
-        // $(this).removeClass("present");
-        // $(this).removeClass("future");
-      }
+  hourBlock.each(function () {
+    var rowTime = parseInt($(this).attr("id"));
+    console.log(Math.floor(rowTime));
+
+    if (currentTime > rowTime) {
+      $(this).addClass("past");
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      console.log($(this));
+    } else if (currentTime < rowTime) {
+      $(this).addClass("future");
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      console.log($(this));
+    } else {
+      $(this).addClass("present");
+      $(this).removeClass("future");
+      $(this).removeClass("past");
+      console.log($(this));
     }
-  );
+  });
+
+  $(function () {
+    //Save button function
+    $(".saveBtn").on("click", function () {
+      var text = $(this).siblings(".col-md-9").val();
+      var time = $(this).parent().attr("id");
+      localStorage.setItem(time, text);
+    });
+
+    $("#8 .col-md-9").val(localStorage.getItem("8"));
+    $("#9 .col-md-9").val(localStorage.getItem("9"));
+    $("#10 .col-md-9").val(localStorage.getItem("10"));
+    $("#11 .col-md-9").val(localStorage.getItem("11"));
+    $("#12 .col-md-9").val(localStorage.getItem("12"));
+    $("#13 .col-md-9").val(localStorage.getItem("13"));
+    $("#14 .col-md-9").val(localStorage.getItem("14"));
+    $("#15 .col-md-9").val(localStorage.getItem("15"));
+  });
 });
